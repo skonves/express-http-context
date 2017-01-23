9e628cb9ee7fe58358f2810ad0710e3383569fb7
Steve Konves <stephen.konves@gmail.com>
2017-01-23 13:40:02 -0700

    Fix bug

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const nsid = 'a6a29a6f-6747-4b5f-b99f-07ee96e32f88';
 
 module.exports = {
 	middleware: function (req, res, next) {
-		if (cls.getNamespace(nsid)) {
-			const ns = cls.createNamespace(nsid);
-			ns.run(() => next());
-		}
+		const ns = cls.getNamespace(nsid) || cls.createNamespace(nsid);
+		ns.run(() => next());
 	},
 	get: function (key) {
 		const ns = cls.getNamespace(nsid);
