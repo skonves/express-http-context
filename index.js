5b8c87b94fda94737af2851e05a2a745aec6dc57
Steve Konves <stephen.konves@gmail.com>
2017-01-23 13:29:21 -0700

    Update namespace access

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const store = require('continuation-local-storage').createNamespace('a6a29a6f-6747-4b5f-b99f-07ee96e32f88');
+const cls = require('continuation-local-storage');
+
+const nsid = 'a6a29a6f-6747-4b5f-b99f-07ee96e32f88';
 
 module.exports = {
 	middleware: function (req, res, next) {
-		store.run(() => next());
+		if (cls.getNamespace(nsid)) {
+			const ns = cls.createNamespace(nsid);
+			ns.run(() => next());
+		}
 	},
 	get: function (key) {
-		return store.get(key);
+		const ns = cls.getNamespace(nsid);
+		return ns.get(key);
 	},
 	set: function (key, value) {
-		return store.set(key, value);
+		const ns = cls.getNamespace(nsid);
+		return ns.set(key, value);
 	}
 }
