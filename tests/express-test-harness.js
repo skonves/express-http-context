16b62312197dac2602a245c519fe2b8feb0edf14
amiram <amiramk@gmail.com>
2018-02-16 00:17:16 +0200

    add test for async/await

--- tests/express-test-harness.js
+++ tests/express-test-harness.js
@@ -3,6 +3,7 @@
 const assert = require('chai').assert;
 const express = require('express');
 const supertest = require('supertest');
+const bluebird = require('bluebird');
 
 const httpContext = require('../index');
 
@@ -57,7 +58,48 @@ describe('express-cls-hooked', function () {
 		});
 	});
 
-	it('returns undefined when key is not found', function (done) {
+  it('maintains unique value across concurrent requests with async/await', function (done) {
+    // ARRANGE
+    const app = express();
+
+    app.use(httpContext.middleware);
+
+    app.get('/test', (req, res) => {
+      const delay = new Number(req.query.delay);
+      const valueFromRequest = req.query.value;
+
+      httpContext.set('value', valueFromRequest);
+
+      const doJob = async () => {
+        await bluebird.delay(delay);
+        const valueFromContext = httpContext.get('value');
+        res.status(200).json({
+          value: valueFromContext
+        });
+      };
+
+      doJob();
+    });
+
+    const sut = supertest(app);
+
+    const value1 = 'value1';
+    const value2 = 'value2';
+
+    // ACT
+    sut.get('/test').query({ delay: 100, value: value1 }).end((err, res) => {
+      // ASSERT
+      assert.equal(res.body.value, value1);
+      done();
+    });
+
+    sut.get('/test').query({ delay: 50, value: value2 }).end((err, res) => {
+      // ASSERT
+      assert.equal(res.body.value, value2);
+    });
+  });
+
+  it('returns undefined when key is not found', function (done) {
 		// ARRANGE
 		const app = express();
 
