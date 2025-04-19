62d0460dc270ac8e9a114bb263ece15056b36862
Steve Konves <stephen.konves@floqast.com>
2025-04-19 15:20:31 -0700

    test: add test to reproduce issue #78
    Co-authored-by: Oliver Lockwood <olockwood@dalet.com>

--- tests/express-test-harness.js
+++ tests/express-test-harness.js
@@ -4,6 +4,7 @@ const express = require('express');
 const supertest = require('supertest');
 
 const httpContext = require('../index');
+const { init, REQUEST_ID_CONTEXT_KEY, REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME } = require('@oliverlockwood/express-http-context-intermediate-library');
 
 describe('express-http-context', function () {
 	it('does not store or return context outside of request', function () {
@@ -221,5 +222,59 @@ describe('express-http-context', function () {
 			expect(res.body.typeOfValueFromContext).toBe('undefined');
 			done();
 		});
+	});
+
+	it('maintains unique value when the library is depended upon both directly and transitively', async () => {
+		// ARRANGE
+		const app = express();
+
+		// this function in the test library makes the following two calls:
+		//   1.  app.use(middleware) and
+		//   2.  httpContext.set(REQUEST_ID_CONTEXT_KEY, <a unique id>)
+		// as can be seen in https://github.com/oliverlockwood/express-http-context-intermediate-library/blob/original-express-http-context/src/index.ts#L13-L19
+		init(app);
+
+		app.get('/', ((req, res) => {
+			httpContext.set('value', req.query['value']);
+
+			res.status(200).json({
+				fred: '123',
+				value: req.query['value'],
+				valueFromContext: httpContext.get('value'),
+				requestId: httpContext.get(REQUEST_ID_CONTEXT_KEY)
+			});
+		}));
+
+		const request = supertest(app);
+
+		// ACT
+		const [response1, response2] = await Promise.all([
+			request.get('/').query({ value: 'value1' }),
+			request.get('/').query({ value: 'value2' }),
+		]);
+
+		// ASSERT
+		expect(response1.body.value).toBe('value1');
+		expect(response2.body.value).toBe('value2');
+
+		expect(response1.header[REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME]?.length).toBe(21);
+		expect(response2.header[REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME]?.length).toBe(21);
+
+		// This is the specific example I had flagged in the Github issues (#26, #78) - where
+		// setting something into the httpContext in a common library, but it's
+		// unusable from within the application code.
+		expect(response1.body.requestId).toBe(response1.header[REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME]);
+		expect(response2.body.requestId).toBe(response2.header[REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME]);
+
+		// These operations also fail, I suspect, because neither of the set/get
+		// functions are usable, because the directly imported AsyncLocalStorage has
+		// not been initialised by a call to `app.use(middleware)` within our code
+		// here.  Effectively this is another manifestation of the same bug -
+		// showing that although the middleware *has* already been initialised in
+		// Express request handler chain, it is not usable because the
+		// AsyncLocalStorage context is not identical for all usages of the
+		// `express-http-context` library code.
+		expect(response1.body.valueFromContext).toBe('value1');
+		expect(response2.body.valueFromContext).toBe('value2');
 	});
 });
